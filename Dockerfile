91700a0d3bd408311c6a5876ca95443c6fe17710
jetsukda <jetsukda.j@gmail.com>
2020-12-17 21:08:49 +0700

    update code

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM pytorch/pytorch
 
 # if you forked EasyOCR, you can pass in your own GitHub username to use your fork
 # i.e. gh_username=myname
-ARG gh_username=JaidedAI
 ARG service_home="/home/EasyOCR"
 
 # Configure apt and install packages
@@ -19,14 +18,7 @@ RUN apt-get update -y && \
     && apt-get clean -y \
     && rm -rf /var/lib/apt/li
 
-# Clone EasyOCR repo
-RUN mkdir "$service_home" \
-    && git clone "https://github.com/$gh_username/EasyOCR.git" "$service_home" \
-    && cd "$service_home" \
-    && git remote add upstream "https://github.com/JaidedAI/EasyOCR.git" \
-    && git pull upstream master
+WORKDIR $service_home
+COPY . /$service_home
 
-# Build
-RUN cd "$service_home" \
-    && python setup.py build_ext --inplace -j 4 \
-    && python -m pip install -e .
+RUN python setup.py build_ext --inplace -j 16 && python -m pip install -e .
--- main.py
+++ main.py
@@ -1,15 +1,59 @@
-from easyocr import Reader
-from easyocr.utils import reformat_input
+import json
+
 import cv2
+import numpy as np
+from pytictoc import TicToc
 
-image = cv2.imread("doc_734_0.jpg")
+from easyocr import Reader
+from flask import Flask, request
 
+timer = TicToc()
 reader = Reader(["th", "en"])
 
-img, img_cv_grey = reformat_input(image)
-horizontal_list, free_list = reader.detect(img)
 
-results = reader.detect(img_cv_grey, horizontal_list, free_list)
+app = Flask(__name__)
 
-for data in results:
-    print(data)
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    file = request.files['image']
+    contents = file.read()
+    img = np.array(bytearray(contents), dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    horizontal, free = reader.detect(img)
+    horizontal = np.array(horizontal, dtype=np.int32).tolist()
+    quad = np.array(free, dtype=np.int32).tolist()
+    return json.loads(json.dumps({'msg': 'success', "hor": horizontal, "quad": quad}))
+
+
+@app.route("/recognize", methods=["POST"])
+def recognize():
+    file = request.files['image']
+    horizontal = request.args.getlist(key="horizontal", type=list)
+    quad = request.args.getlist(key="quad", type=list)
+
+    horizontal = np.array(horizontal)
+    quad = np.array(quad)
+    contents = file.read()
+    img = np.array(bytearray(contents), dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    result = reader.recognize(img, horizontal, quad)
+    return json.loads(json.dumps({'msg': 'success', "results": horizontal}))
+
+
+@app.route("/readtext", methods=["POST"])
+def readtext():
+    file = request.files['image']
+    contents = file.read()
+    img = np.array(bytearray(contents), dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    results = reader.readtext(img)
+    output = []
+    for loc, text, prob in results:
+        data = {"text": text, "loc": loc, "prob": prob}
+        output.append(data)
+    return json.loads(json.dumps({'msg': 'success', "results": str(output)}))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host=18, port=8666)
